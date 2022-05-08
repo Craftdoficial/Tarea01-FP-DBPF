a54860ec4de4f264a41448203a16ebeecef8cf91
Craftdoficial <103528700+Craftdoficial@users.noreply.github.com>
2022-05-07 22:34:40 -0500

    Add files via upload

---
+++ EstCondicional/Plazavea.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Plazavea
+ */
+public class Plazavea {
+
+    static Scanner lt=new Scanner(System.in);
+    public static void main(String[] args) {
+        //Declarar variables
+        double totalapagar;
+        int n;
+        //Datos de entrada
+        System.out.println("Ingrese la cantidad de horas a usar" );
+        n=lt.nextInt();
+        //Proceso
+        if (n<=2) {
+            totalapagar= n*5;
+        } else if (n<=5) {
+            totalapagar=((n-2)*4)+10;
+        } else if (n<=10) {
+            totalapagar=((n-5)*3)+22;
+        }else{
+            totalapagar=((n-10)*2)+37;
+                } { 
+        }
+        //Datos de salida
+        System.out.println("La cantidad a pagar por es de"+totalapagar);
+    }
+}
